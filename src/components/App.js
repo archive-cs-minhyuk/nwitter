1acdbc27c31c05ecbad1e307de62f471d5c79e70
nikestussy <smh0706@kaist.ac.kr>
2021-01-09 20:42:29 +0900

    3.3 RealTime Nweets

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false); // 시작할 때 firebase initialization을 기다려주기 위함
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [userObj, setUserObj] = useState(null); //나중에 사용할 수 있도록 현재 유저 정보 저장
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       //onAuthStateChanged: Auth상태 변할 때마다. (event listener 같은것)
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         //log out 될 경우도 있으니!
         setIsLoggedIn(false);
@@ -19,7 +21,11 @@ function App() {
   }, []); //empty array: component mount될 때만 사용됨.
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,25 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]); //nweets 가져오기 위함
+  useEffect(() => {
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      //database의 변화를 감지
+      const nweetArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setNweets(nweetArray); //array를 만든 후 setNweets로 한 번에 설정
+    });
+  }, []); //mount 될 때만
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("nweets").add({
-      nweet: nweet,
+      text: nweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid, //누가 이 nweet을 만들었는가?
     });
     setNweet(""); //submit 이후 empty로 바꾸는 과정
   };
@@ -29,6 +41,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div id={nweet.id}>
+            <h4>{nweet.text}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
