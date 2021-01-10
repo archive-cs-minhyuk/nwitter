df9ba3bfe75e3cca4e4696f968d8832c00cc64d5
nikestussy <smh0706@kaist.ac.kr>
2021-01-10 13:58:45 +0900

    5.1 Update Profile finish

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,39 @@
 import { authService } from "fbase";
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory(); //주소 돌아갈 수 있게 해주는 hook
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/"); //log out 이후 "/"로 돌아감
   };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
